7a8dcc9b2c3d214e44358f155564f7ac8ffafe35
임유미 <dladbal24@sookmyung.ac.kr>
2026-07-15 21:28:23 +0900

    test: 테스트 코드 추가

--- Assets/Scripts/UISystemScripts/PopupScripts/Test 코드/PopupTest.cs
+++ Assets/Scripts/UISystemScripts/PopupScripts/Test 코드/PopupTest.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PopupCallerTest : MonoBehaviour
+public class PopupTest : MonoBehaviour
 {
     public void ShowPopup()
     {
